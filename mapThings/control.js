e4fa880cc22815d6c09c922a45e9f34bc1d870c8
tamban topayakar <onepiece30233@gmail.com>
2019-07-17 01:25:14 +0700

    update animation moving object

--- mapThings/control.js
+++ mapThings/control.js
@@ -1,8 +1,12 @@
 var jsonData;
 let numOfCar;
-var markerArray = []
+var truckShown = new Object()
 var map;
 var layers = longdo.Layers
+var thisTruck;
+var locWay;
+var ready = false;
+var speedAnimation
 
 function init2() {
     
@@ -12,20 +16,49 @@ function init2() {
     
     $.getJSON('http://www.whateverorigin.org/get?url='+encodeURIComponent('http://usermap.longdo.com/montri/MM-WifiBox/data/2018-05-06.json')+'&callback=?', (res)=> {
         jsonData = res.contents [0]
-        updateLoc(jsonData)
+        loadData()
         
     });    
 }
 
-function updateLoc() {
+function loadData() {
     for(index in jsonData) {
 //        console.log(jsonData[index]);
-        let carObj = jsonData[index]
-        addTruck({
-            lon: carObj.lon,
-            lat: carObj.lat
-        })
+        let truckObj = jsonData[index]
+        let id = truckObj["imei"]
+        let loc = {
+            lon : truckObj.lon,
+            lat : truckObj.lat
+        }
+        if (!(id in truckShown)) {
+            truckShown[id] = {
+                des : [truckObj],
+                loc : [loc]
+            }
+//            console.log(truckShown)
+    //        var ar = truckShown["tst"].split(" ")
+    //        var date = ar[0]
+    //        var [year, month, day] = date.split("-")
+    //        var time = ar[1]
+    //        var [hour,min,sec] = time.split(":")
+        } else {
+           truckShown[id].des.push(truckObj)
+           truckShown[id].loc.push(loc)
+//           truckShown[id].sort(compareTruck)
+        }
     }
+    addAllTruck()
+}
+
+function compareTruck(trA, trB) {
+    let sts1 = trA.tst
+    let sts2 = trB.tst
+    if(sts1 < sts2) {
+        return 1
+    } else {
+        return -1
+    }
+    
 }
     
 function addTruck(loc,id) {
@@ -35,37 +68,56 @@ function addTruck(loc,id) {
             url : 'https://map.longdo.com/mmmap/images/pin_mark.png', 
             offset : {x: 12, y: 45}
         }, 
+        detail : "moving",
+        weight: longdo.OverlayWeight.Top,
         clickable: true,
-        draggable: true
+        draggable: false
     });
-    let marker2 = new longdo.Marker(loc, {
-        title : "BCar",
-        icon : {
-            url : 'https://map.longdo.com/mmmap/images/pin_mark.png', 
-            offset : {x: 12, y: 45}
-        }, 
-        clickable: true,
-        draggable: true
-    });
-    map.Overlays.add(marker) 
-    console.log(marker.location().longitude)
-//    addMarker(marker)
-//    checkMarker(marker)
+
+    map.Overlays.drop(marker)
+    return marker
 }
 
-function moveLoc(data) {
-    jsonData = data
+function addAllTruck() {
+    for (index in truckShown) {
+        locWay = truckShown[index].loc
+        thisTruck = addTruck(locWay[0],index)
+        ready = true
+    }
+//       console.log(marker.location())
+//        map.Overlays.pathAnimation(marker, new longdo.Polyline([{ lon: 102, lat: 18 }, { lon: 98, lat: 17 }, { lon: 99, lat: 14 }]));
+//       map.Overlays.pathAnimation(marker, new longdo.Polyline(locWay));
 }
 
-function addMarker(marker) {
-    markerArray.push(marker)
+function moveTruck() {
+    if(!ready) {
+        alert('not ready to move')
+        return;
+    }
+
+    if(thisTruck.location() !== locWay[0] && thisTruck.location() !== locWay.slice(-1)[0]) {
+        alert('why u press butt');
+        return;
+    }
+    if(thisTruck.location() === locWay.slice(-1)[0]) {
+        alert('will start again');
+        thisTruck.move(locWay[0], true);    
+    }
+    
+    map.Overlays.pathAnimation(thisTruck, new longdo.Polyline(locWay));
+}
+
+function changeSpeed() {
+    let speedBar = document.getElementById('speedAnimation');
+    speedAnimation = speedBar.value/10;
 
 }
+
+
 
 function checkMarker(marker) {
-    console.log(markerArray.findIndex( (element)=> {
-        return marker === element
-    }))
-    
+    var index = markerArray.findIndex( (element)=> {
+        return marker.location.lon === element.location.lon && marker.location.lat === element.location.lat
+    })
 }
 
